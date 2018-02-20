7eaeb8baff0f3ee2f93b5b8d04440120ed312148
tenzin2017 <tenzinleo@gmail.com>
2018-02-19 23:52:25 -0500

    postgresql issue fixed, recreate db and add new col to devise users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180210193654) do
+ActiveRecord::Schema.define(version: 20180220043650) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -48,6 +51,7 @@ ActiveRecord::Schema.define(version: 20180210193654) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
